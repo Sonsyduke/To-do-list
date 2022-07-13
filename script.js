a585e7b0688b96e654ec9ea20c87a1ede479230d
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-13 09:37:40 +0200

    Added the new and list feature

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const todoList = [];
+const todoList = ["Wash Rolly", "Walk Rolly"];
 let userAnswer = prompt("What do you want to do?").toLowerCase();
 
 while (userAnswer !== "quit" && userAnswer !== "q") {
+  if (userAnswer === "list") {
+    console.log(`*****************`);
+    for (let i = 0; i < todoList.length; i++) {
+      console.log(`${i} : ${todoList[i]}`);
+    }
+    console.log(`*****************`);
+  } else if (userAnswer === "new") {
+    const newTodo = prompt(`What is the new todo?`);
+    todoList.push(newTodo);
+    console.log(`${newTodo} added to the list`);
+  }
   userAnswer = prompt(`what would you like todo?`);
 }
 
